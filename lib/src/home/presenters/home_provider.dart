a00c76ec2fd1377f6ce3794d4584d676102e4307
3bdsyd <mr.aaa48@gmail.com>
2024-05-21 19:14:31 +0300

    finish app

--- lib/src/home/presenters/home_provider.dart
+++ lib/src/home/presenters/home_provider.dart
@@ -1,6 +1,5 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:path/path.dart';
 import 'package:pull_to_refresh_flutter3/pull_to_refresh_flutter3.dart';
 import 'package:todo_app/core/helper/database.dart';
 import 'package:todo_app/core/helper/failures_handling.dart';
